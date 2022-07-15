cc3888388bd1a8d9452466a627a2fab4989049d4
Jake Boerjan <jacob.boerjan@gmail.com>
2022-07-15 13:14:05 -0500

    First commit

---
+++ src/com/pluralsight/calcengine/MathProcessing.java
@@ -0,0 +1,9 @@
+package com.pluralsight.calcengine;
+
+public interface MathProcessing {
+
+    String SEPARATOR = " ";
+    String getKeyWord();
+    double doCalculation(double leftVal, double rightVal);
+
+}
